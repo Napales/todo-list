a7f41a22fced8792aeeff21802045eb8c64fc0d9
Novikov Sergey <@napos_ns@mail.ru>
2025-06-26 21:55:52 +0600

    Добавил новую модель в базу данных

--- To_do_list/settings.py
+++ To_do_list/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'To_do_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_db',
+        'USER': 'postgres',
+        'PASSWORD': '4644',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
