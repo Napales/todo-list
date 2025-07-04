b7767cc1786d8996421f0abb7ff300e914511270
Novikov Sergey <@napos_ns@mail.ru>
2025-07-04 16:09:04 +0600

    Добавил изменение задачи, детальный просмотр и изменение перенес в форму HTML

--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponseRedirect
+
 from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.models import Task, status_choices
 
 
 def index(request):
-    tasks = Task.objects.order_by('-complete_date')
+    tasks = Task.objects.order_by('-compelet_date')
     return render(request, 'index.html', {"tasks": tasks})
 
 
@@ -13,15 +13,39 @@ def create_task(request):
     if request.method == "POST":
         description = request.POST.get("description")
         status = request.POST.get("status")
-        complete_date = request.POST.get("complete_date")
+        compelet_date = request.POST.get("compelet_date")
         detail_description = request.POST.get("detail_description")
-        Task.objects.create(description=description, status=status, complete_date=complete_date,
+        Task.objects.create(description=description, status=status, compelet_date=compelet_date,
                             detail_description=detail_description)
         return redirect('index')
     else:
         return render(request, 'create_task.html', {'status_choices': status_choices})
 
 
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.delete()
+        return redirect('index')
+    else:
+        return render(request, 'task_delete.html', {'task': task})
+
+
 def detail_task(request, pk):
     task = get_object_or_404(Task, pk=pk)
     return render(request, 'task_detail.html', {'task': task})
+
+def update_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.description = request.POST.get("description")
+        task.status = request.POST.get("status")
+        task.compelet_date = request.POST.get("compelet_date")
+        task.detail_description = request.POST.get("detail_description")
+        task.save()
+        return redirect('index')
+    else:
+        return render(request, 'update_task.html', {'status_choices': status_choices, 'task': task})
+
+
+
