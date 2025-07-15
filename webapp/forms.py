a5d018335a11ee50d289f4b6806a0555dee60144
Novikov Sergey <@napos_ns@mail.ru>
2025-07-15 16:02:58 +0600

    Сделал чекбокс для типов с галочками

--- webapp/forms.py
+++ webapp/forms.py
@@ -5,15 +5,18 @@ from webapp.models import Task, Type, Status
 
 
 class TaskForm(forms.ModelForm):
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(),
-                                          widget=forms.SelectMultiple(attrs={'class': 'form-control'}))
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all()),
     status = forms.ModelChoiceField(queryset=Status.objects.all())
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
+            if not isinstance(field.widget, widgets.CheckboxSelectMultiple):
+                field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Task
         fields = ['description', 'detail_description', 'status', 'type']
+        widgets = {
+            'type' : forms.CheckboxSelectMultiple(),
+        }
