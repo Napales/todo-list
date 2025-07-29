841ea18071e0187893fc63d0d37de88fdd66a603
Novikov Sergey <@napos_ns@mail.ru>
2025-07-29 15:45:26 +0600

    Добавил в регистрацию валидатор и обязательное поле с почтой

--- accounts/forms/user_creatiion.py
+++ accounts/forms/user_creatiion.py
@@ -1,9 +1,22 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
 class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
 
     class Meta(UserCreationForm.Meta):
         fields = ('username', 'password1', 'password2', 'first_name', 'last_name', 'email')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        if not first_name and not last_name:
+            raise ValidationError('Заполните одно из полей "first_name" или "last_name" ')
+        return cleaned_data
+
